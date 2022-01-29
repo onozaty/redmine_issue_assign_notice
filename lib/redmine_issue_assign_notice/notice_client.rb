c779a3852360b74a2557fae0bd4608980bab9aca
onozaty <onozaty@gmail.com>
2022-01-30 00:02:08 +0900

    Change webhook calls to asynchronous #17

--- lib/redmine_issue_assign_notice/notice_client.rb
+++ lib/redmine_issue_assign_notice/notice_client.rb
@@ -10,15 +10,24 @@ module RedmineIssueAssignNotice
         client = HTTPClient.new
         client.ssl_config.cert_store.set_default_paths
         client.ssl_config.ssl_version = :auto
-        res = client.post url, message.to_json, {'Content-Type' => 'application/json; charset=UTF-8'}
+        conn = client.post_async url, message.to_json, {'Content-Type' => 'application/json; charset=UTF-8'}
 
-        if !HTTP::Status.successful?(res.status) 
-          Rails.logger.warn("[RedmineIssueAssignNotice] Failed request to #{url}")
-          Rails.logger.warn(res.inspect)
-          return
+        Thread.new do
+          begin
+            res = conn.pop
+            if !HTTP::Status.successful?(res.status) 
+              Rails.logger.warn("[RedmineIssueAssignNotice] Failed request to #{url}")
+              Rails.logger.warn(res.inspect)
+              return
+            end
+
+            Rails.logger.debug "[RedmineIssueAssignNotice] NoticeClient#notice success"
+
+          rescue Exception => e
+            Rails.logger.warn("[RedmineIssueAssignNotice] Failed request to #{url}")
+            Rails.logger.warn(e.inspect)
+          end
         end
-
-        Rails.logger.debug "[RedmineIssueAssignNotice] NoticeClient#notice success"
 
       rescue Exception => e
         Rails.logger.warn("[RedmineIssueAssignNotice] Failed request to #{url}")
